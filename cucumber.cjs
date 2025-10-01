7926e638782f9ac36947b41e8f1600c363983b16
juanpabarriosr <32270243+juanpabarriosr@users.noreply.github.com>
2025-10-01 17:51:49 -0500

    mejora en cucumber.base.js

--- cucumber.cjs
+++ cucumber.cjs
@@ -2,5 +2,13 @@
 const { createConfig } = require('@automation/web-automation-framework/cucumber/cucumber.base');
 
 module.exports = createConfig({
-  // Solo customizaciones específicas de este proyecto si las hay
+  paths: {
+    world: 'support/world.ts',
+    hooks: 'support/hooks.ts',
+    steps: [
+      'features/steps/**/*.ts',
+      'test/steps/**/*.ts',
+      'test/step-definitions/**/*.ts'
+    ]
+  }
 });
