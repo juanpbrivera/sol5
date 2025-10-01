a51d01fd0a8eaea131f2b279a75fe45f4024470d
juanpabarriosr <32270243+juanpabarriosr@users.noreply.github.com>
2025-10-01 17:14:32 -0500

    funciona navegadores

--- cucumber.cjs
+++ cucumber.cjs
@@ -3,15 +3,16 @@ module.exports = {
   default: {
     requireModule: ['ts-node/register/transpile-only'],
     require: [
-      // Registra el World directamente desde support
       'support/world.ts',
-      // tus hooks y steps
       'support/hooks.ts',
       'features/steps/**/*.ts'
     ],
     format: ['progress', 'json:reports/cucumber.json'],
     publishQuiet: true,
     parallel: 1,
-    worldParameters: { env: 'cert' }
+    worldParameters: { 
+      env: 'cert',
+      browser: process.env.BROWSER  // Pasa el browser como parámetro
+    }
   }
 };
